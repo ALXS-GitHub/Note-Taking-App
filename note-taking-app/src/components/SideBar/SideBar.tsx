3f5fc425611b30c59f89d642b0698f85e70dfadc
ALXS <alxs.mnch@gmail.com>
2024-01-22 12:17:09 +0100

    fix

--- note-taking-app/src/components/SideBar/SideBar.tsx
+++ note-taking-app/src/components/SideBar/SideBar.tsx
@@ -17,6 +17,9 @@ function SideBar() {
         createNote(
             "Untitled Note",
             "This is a new note",
+            new Date().getTime(),
+            false,
+            "default"
         ).then((note) => {
             setNotes([note, ...notes]);
         });
